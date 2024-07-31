e997f4b3238d8a5fdc93ee57145c7d546fb3652b
Aimee Taylor <AimeeJATDeveloper@outlook.com>
2024-07-31 20:30:24 +0100

    Updated SQL query and removed one column from HTML table as I don't think it's needed

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ db = sqlite3.connect("static/game/highscores.db", check_same_thread=False)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    highscores = db.execute("SELECT * FROM scores;")
+    highscores = db.execute("SELECT name, score FROM scores ORDER BY score DESC;")
     return render_template("index.html", highscores=highscores)
